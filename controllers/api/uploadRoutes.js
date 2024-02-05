60d139d864b91f14b961a1634991b75e455883a2
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-05 14:26:54 -0800

    able to grab data from db using musicRoutes.js, able to upload new songs using uploadRoutes.js

--- controllers/api/uploadRoutes.js
+++ controllers/api/uploadRoutes.js
@@ -6,4 +6,25 @@ const { Song, Playlist } = require('../../models');
 /playlists POST
 */
 
+router.post('/song', async (req, res) => {
+    try{
+        console.log("Uploading!");
+        const songData = await Song.create(req.body);
+        res.status(200).json(songData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+router.post('/playlist', async (req, res) => {
+    try{
+        console.log("Uploading!");
+        const playlistData = await Playlist.create(req.body);
+        res.status(200).json(playlistData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
