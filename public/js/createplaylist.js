19cd41e54865346952ee3c0ed9f13c99dc1ed23d
sebjustseb <sebjustseb@gmail.com>
2024-02-07 14:30:30 -0800

    unfinished playlisthandler

--- public/js/createplaylist.js
+++ public/js/createplaylist.js
@@ -1,3 +1,9 @@
 const createPlaylistHandler = async (event) => {
     event.preventDefault();
+
+    const play_name = document.querySelector('#playlist-name').value.trim();
+    let song_id = document.querySelector('#songid').value.trim();
+    console.log(song_id);
 };
+
+document.querySelector('#cpbutton').addEventListener('click', addSongHandler)
