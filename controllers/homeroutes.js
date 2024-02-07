21fbc3df2559a9ceb6f5dbb5fb06330ab7b0216f
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-07 10:18:41 -0800

    moved get routes for songs and playlists into homeRoutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -26,5 +26,41 @@ router.get('/profile', withAuth, async (req, res) => {
     }
 })
 
+// /songs FIND ALL SONGS IN DB
+router.get('/songs', async (req, res) => {
+    try {
+        const songData = await Song.findAll();
+
+        const songs = songData.map(sg => sg.get({ plain: true }));
+        //res.status(200).json(songData);
+        
+        res.render('songs', {
+            songs
+        });
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// /playlists
+router.get('/playlists', async (req, res) => {
+    try {
+        const playlistData = await Playlist.findAll({
+            include: [{ model: Song, through: PlaylistSongs }, { model: User }]
+        });
+
+        const playlists = playlistData.map(pd => pd.get({ plain: true }));
+        //res.status(200).json(playlists);
+
+        res.render('playlist', {
+            playlists
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
