885fa8a0259d68cade41bccd7242abc2695d0fc3
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-07 14:13:25 -0800

    created post route to add playlist

--- controllers/api/musicRoutes.js
+++ controllers/api/musicRoutes.js
@@ -10,6 +10,30 @@ router.post('/addsong', async(req, res) => {
     }
 });
 
+router.post('/playlist', async (req, res) => {
+    try {
+        console.log("Uploading!");
+        const playlistData = await Playlist.create({play_name:req.body.play_name});
+        const songs = req.body.songs;
+        const playlist_id = playlistData.id
+        for (const song_id of songs){
+            PlaylistSongs.create({playlist_id, song_id})
+        }
+        /*
+                for loop (Song.id){
+                    for each Song.id{
+                        PlaylistSongs.create(playlistData.id, Song.id)
+                    }
+                }
+        
+                have a button on a js add button, this button pushes the id of associated song to an array
+                have a CREATE PLAYLIST button, this button sends the array of id's to this post request route 
+        */
+        res.status(200).json(playlistData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
