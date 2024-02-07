01ce91b82caf6d1bc92c2f75d4e389ba6bf0c397
sebjustseb <sebjustseb@gmail.com>
2024-02-07 14:04:11 -0800

    createplaylist actually connected

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -73,7 +73,7 @@ router.get('/playlists', async (req, res) => {
 });
 
 
-router.get('/createplaylists', async (req, res) => {
+router.get('/createplaylist', async (req, res) => {
     try {
         const songData = await Song.findAll({
             order: [['title', 'ASC']],
